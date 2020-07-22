c75b2807b5500f7d8dad2c220a93064e83196e9b
Patrick <42988420+patrick8link@users.noreply.github.com>
2020-07-22 18:06:31 +0900

    Update and rename createVNF.py to createVNFD.py

--- Hackathon-108/openstack/createVNFD.py
+++ Hackathon-108/openstack/createVNFD.py
@@ -2,8 +2,8 @@ import os
 import time
 
 os.system("tacker vnfd-create --vnfd-file time_based_firewall_vnfd.yaml time_based_firewall_vnfd")
-time.sleep(5)
+time.sleep(10)
 
 os.system("tacker vnfd-create --vnfd-file url_filtering_vnfd.yaml url_filtering_vnfd")
-time.sleep(5)
+time.sleep(10)
 
