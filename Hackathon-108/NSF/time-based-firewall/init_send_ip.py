78d1ca20b1282422b275c3c09be613324a4afc8e
Patrick <42988420+patrick8link@users.noreply.github.com>
2020-08-13 18:31:08 +0900

    Update init_send_ip.py

--- Hackathon-108/NSF/time-based-firewall/init_send_ip.py
+++ Hackathon-108/NSF/time-based-firewall/init_send_ip.py
@@ -3,11 +3,11 @@ import socket
 import sys
 from time import ctime
 
-HOST = '10.0.0.6'
+HOST = '10.0.0.6' #EDIT TO YOUR DMS IP
 PORT = 55561
 ADDR = (HOST,PORT)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('115.145.178.180',0))
+s.connect(('115.145.178.180',0)) #EDIT TO YOUR OPENSTACK IP
 print (socket.gethostname())
 print (s.getsockname()[0])
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
