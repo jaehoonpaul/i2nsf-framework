320500e5cb68f067815036cbdbbdb37839e613b8
Patrick <42988420+patrick8link@users.noreply.github.com>
2020-08-13 18:30:36 +0900

    Update openstack_server.py

--- Hackathon-108/openstack/openstack_server.py
+++ Hackathon-108/openstack/openstack_server.py
@@ -9,7 +9,7 @@ def receive_nsf_request():
     while True:
         server_socket = socket.socket()
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket.bind(('115.145.178.180', 55560))
+        server_socket.bind(('115.145.178.180', 55560)) #EDIT TO YOUR OPENSTACK IP ADDRESS
         server_socket.listen(5)
         client_socket, addr = server_socket.accept()
         data = client_socket.recv(1024)
